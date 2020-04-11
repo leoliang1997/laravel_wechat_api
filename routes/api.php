8fdde168e39a73febccf911120f93e838a0b1bc5
leoliang <leoliang@futunn.com>
2020-04-11 18:41:09 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::namespace('Api')->group(function () {
 
 Route::middleware('auth:api')->namespace('Api')->group(function () {
     Route::get('info', 'UsersController@info')->name('users.info');
-    Route::post('create-lesson', 'LessonsController@create')->name('lessons.create');
+    Route::post('create', 'LessonsController@create')->name('lessons.create');
     Route::get('lesson-list', 'LessonsController@lessonList')->name('lessons.lessonList');
     Route::post('join', 'LessonsController@join')->name('lessons.join');
     Route::get('my-lesson', 'LessonsController@myLession')->name('lessons.myLesson');
